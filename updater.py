02f74008f7aa3781868522f258c24fd49d1dbe16
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-08-18 21:17:40 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -6,6 +6,7 @@ from colorama import init
 from colorama import Fore, Back, Style
 import random
 
+
 def update_client_version(version):
     with open("version.txt", "r") as vnum:
         if vnum.read() != version:
@@ -13,9 +14,17 @@ def update_client_version(version):
         else:
             return False
 
+
 def main():
     try:
         version = urlopen("https://raw.githubusercontent.com/httpshotmaker/technowlogger_with_cryptor/master/version.txt").read()
+        version = str(version)
+        version = version.replace('\\', '')
+        version = version.replace("'", "")
+        version = version.replace('r', '')
+        version = version.replace('n', '')
+        version = version.replace('v', '')
+        version = version.replace('b', '')
     except Exception as e:
         print(f"{Fore.RED}[!] Unable to Fletch Origin version.txt")
         print("[!] Please Check Your Internet Connection!")
@@ -24,10 +33,12 @@ def main():
         
     if update_client_version(version) is True:
         subprocess.call(["git", "pull", "origin", "master"])
+        with open('version.txt', 'w') as upd:
+            upd.write(version)
         subprocess.call(["git", "add", "."])
         id = random.randint(1, 9999999999)
         subprocess.call(["git", "commit", "-m", f"update_{id}"])
-        return f"{Fore.GREEN}[+] Updated to latest version: v{version}..\n[*] Update version.txt"
+        return f"{Fore.GREEN}[+] Updated to latest version: v{version}.."
     else:
         return f"{Fore.GREEN}[*] You are already up to date with git origin master :)"
 
